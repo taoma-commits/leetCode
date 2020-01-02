0afbadfe78546db379dfc35de757ed9a30e9a03f
taoma-commits <matao271@gmail.com>
2020-01-02 10:28:03 -0500

    problem 121

---
+++ [121]bestTimeToBuyAndSellStock.java
@@ -0,0 +1,16 @@
+/*
+Runtime: 0 ms, faster than 100.00% of Java online submissions for Best Time to Buy and Sell Stock.
+Memory Usage: 36.8 MB, less than 100.00% of Java online submissions for Best Time to Buy and Sell Stock.
+*/
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        int min = Integer.MAX_VALUE;
+        int profit = 0;
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] - min > profit) profit = prices[i] - min;
+            else if (prices[i] < min) min = prices[i];
+        }
+        return profit;
+    }
+}
