cd3d71e76471f3d6466411f043cf299a18b9136c
taoma-commits <matao271@gmail.com>
2020-01-10 16:28:25 -0500

    1/10/20

---
+++ [198]houseRobber.java
@@ -0,0 +1,24 @@
+/*
+Runtime: 0 ms, faster than 100.00% of Java online submissions for House Robber.
+Memory Usage: 34 MB, less than 100.00% of Java online submissions for House Robber.
+@dynamicProgramming
+*/
+
+
+
+class Solution {
+    public int rob(int[] nums) {
+        int len = nums.length;
+        if (len == 0) return 0;
+        if (len == 1) return nums[0];
+        int sum, former;
+        former = sum = 0;
+        int latter = nums[0];
+        for (int i = 1; i < len; i++) {
+            sum = former + nums[i];
+            former = latter;
+            latter = Math.max(sum, latter);
+        }
+        return latter;
+    }
+}
