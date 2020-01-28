f66b3b7121095a9c0fc8444ba000c34b7cd71864
taoma-commits <matao271@gmail.com>
2020-01-28 16:29:43 -0500

    1/28/20

---
+++ [11]containerWithMostWater.java
@@ -0,0 +1,27 @@
+/*
+Runtime: 1 ms, faster than 100.00% of Java online submissions for Container With Most Water.
+Memory Usage: 41.1 MB, less than 13.46% of Java online submissions for Container With Most Water.
+*/
+
+//tag: array, two pointers.
+
+class Solution {
+    public int maxArea(int[] height) {
+        int i = 0, j = height.length - 1, max = 0;
+        while (i < j) {
+            if (height[i] <= height[j]) {
+                max = Math.max(max, height[i] * (j - i));
+                int k = 1;
+                while (height[i + k] <= height[i] && k < j - i) k++;
+                i += k;
+            }
+            else {
+                max = Math.max(max, height[j] * (j - i));
+                int k = 1;
+                while (height[j - k] <= height[j] && k < j - i) k++;
+                j -= k;
+            }
+        }
+        return max;
+    }
+}
