4964e15449980221d1cf98ec9da5561c6655852b
taoma-commits <matao271@gmail.com>
2020-01-30 00:59:40 -0500

    1/29/20

---
+++ [34]firstAndLastPositionInSortedArray.java
@@ -0,0 +1,68 @@
+/*
+Runtime: 0 ms, faster than 100.00% of Java online submissions for Find First and Last Position of Element in Sorted Array.
+Memory Usage: 42.9 MB, less than 99.29% of Java online submissions for Find First and Last Position of Element in Sorted Array.
+*/
+
+//tag : array, binary search, two-time binary search
+
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int[] range = new int[] {-1, -1};
+        if (nums == null || nums.length == 0) return range;
+        int i = binary(nums, target, true);
+        int j = binary(nums, target, false);
+        if (i < nums.length && nums[i] == target) {
+            range[0] = i;
+            range[1] = j - 1;
+        }
+        return range;
+    }
+
+    private int binary(int[] nums, int target, boolean left) {
+        int low = 0, high = nums.length;
+        while (low < high) {
+            int mid = (low + high) / 2;
+            if (target < nums[mid] || (target == nums[mid] && left)) high = mid;
+            else low = mid + 1;
+        }
+        return high;
+    }
+}
+
+
+/* Notes:
+* corner cases
+* set low = mid + 1 to escape deadlock in binary search
+* set high = nums.length instead of nums.length - 1
+* one array for range
+*/
+
+/*
+Runtime: 0 ms, faster than 100.00% of Java online submissions for Find First and Last Position of Element in Sorted Array.
+Memory Usage: 42.8 MB, less than 100.00% of Java online submissions for Find First and Last Position of Element in Sorted Array.
+*/
+
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int[] range = new int[] {-1, -1};
+        if (nums == null || nums.length == 0) return range;
+        double left = target - 0.5, right = target + 0.5;
+        int i = binary(nums, left);
+        int j = binary(nums, right);
+        if (i < nums.length && nums[i] == target) {
+            range[0] = i;
+            range[1] = j - 1;
+        }
+        return range;
+    }
+
+    private int binary(int[] nums, double target) {
+        int low = 0, high = nums.length;
+        while (low < high) {
+            int mid = (low + high) / 2;
+            if (target < nums[mid]) high = mid;
+            if (target > nums[mid]) low = mid + 1;
+        }
+        return high;
+    }
+}
