446046a974d8c60b316eebce439158debdec9df5
taoma-commits <matao271@gmail.com>
2020-01-31 12:13:08 -0500

    1/31/20

--- Array/[134]gasStation.java
+++ Array/[134]gasStation.java
@@ -2,6 +2,7 @@
 Runtime: 0 ms, faster than 100.00% of Java online submissions for Gas Station.
 Memory Usage: 38.7 MB, less than 21.57% of Java online submissions for Gas Station.
 */
+
 class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {
         int total = 0, start = 0, sum = 0;
@@ -17,3 +18,6 @@ class Solution {
         else return start;
     }
 }
+
+// tag: greedy
+// idea: realize that total sum >= 0 guarantees existence of a solution.
