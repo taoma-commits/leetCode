17311eda2ad24ac859c743d2d9e9b8a7acaeb162
taoma-commits <matao271@gmail.com>
2020-01-31 12:11:29 -0500

    1/31/20

---
+++ [134]gasStation.java
@@ -0,0 +1,19 @@
+/*
+Runtime: 0 ms, faster than 100.00% of Java online submissions for Gas Station.
+Memory Usage: 38.7 MB, less than 21.57% of Java online submissions for Gas Station.
+*/
+class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int total = 0, start = 0, sum = 0;
+        for (int i = 0; i < gas.length; i++) {
+            sum += gas[i] - cost[i];
+            total += gas[i] - cost[i];
+            if (sum < 0) {
+                sum = 0;
+                start = i + 1;
+            }
+        }
+        if (total < 0) return -1;
+        else return start;
+    }
+}
