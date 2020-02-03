9c06e860092419ead9b74ad41dbc08f95939d7c8
taoma-commits <matao271@gmail.com>
2020-02-03 13:40:28 -0500

    2/3/20

--- Array/twoPointers/[245]shortestWordDistanceIII.java
+++ Array/twoPointers/[245]shortestWordDistanceIII.java
@@ -1,15 +1,15 @@
 class Solution {
     public int shortestWordDistance(String[] words, String word1, String word2) {
-        long dist = Integer.MAX_VALUE, idx1 = dist, idx2 = - dist;
+        long dist = Integer.MAX_VALUE, idx1 = dist, idx2 = - dist; //note 1
         for (int i = 0; i < words.length; i++) {
             if (word1.equals(words[i])) idx1 = i;
             if (word2.equals(words[i])) {
-                if (word1.equals(word2)) idx1 = idx2;
+                if (word1.equals(word2)) idx1 = idx2; //note 3
                 idx2 = i;
             }
             dist = Math.min(dist, Math.abs(idx2 - idx1));
         }
-        return (int) dist;
+        return (int) dist;// note 2
     }
 }
 /*
@@ -17,5 +17,6 @@ class Solution {
 * notes:
       1. let dist, idx1, and idx2 be long type can avoid overflow
       2. cast dist back to int type by (int) distance
+      3. one line code takes care of special case.
 * companies: LinkedIn 2, Microsoft 2, Adobe, Walmart lab, Google.
 */
