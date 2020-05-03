1404271aa9c2ebcd727f52a430fdcd20c9f5b332
taoma-commits <matao271@gmail.com>
2020-05-02 23:32:40 -0400

    5/2/20

--- Backtracking/[291]wordPatternII.java
+++ Backtracking/[291]wordPatternII.java
@@ -10,18 +10,29 @@ class Solution {
   private void backtrack(String p, String s, int i, int j) {
     if (i == p.length()) {
       if (j == s.length()) res = true;
-      return; 
+      return;
     }
-    for (int k = j; k < s.length(); k++) {
-      System.out.println(p.charAt(i));
-      System.out.println(s.substring(j, k + 1));
-      if (P[p.charAt(i) - 'a'] != S.getOrDefault(s.substring(j, k + 1), 0)) continue;
-      if (P[p.charAt(i) - 'a'] == 0) {
-        P[p.charAt(i) - 'a'] = i + 1;
-        S.put(s.substring(j, k + 1), i + 1);
+    if (!res) {
+      for (int k = j; k < s.length() - p.length() + i + 1; k++) {
+        // System.out.println(p.charAt(i));
+        // System.out.println(s.substring(j, k + 1));
+        if (P[p.charAt(i) - 'a'] != S.getOrDefault(s.substring(j, k + 1), 0)) continue;
+        if (P[p.charAt(i) - 'a'] == 0) {
+          P[p.charAt(i) - 'a'] = i + 1;
+          S.put(s.substring(j, k + 1), i + 1);
+          backtrack(p, s, i + 1, k + 1);
+          P[p.charAt(i) - 'a'] = 0;
+          S.remove(s.substring(j, k + 1));
+        } else {
+          backtrack(p, s, i + 1, k + 1);
+        }
       }
-      backtrack(p, s, i + 1, k + 1);
-      S.remove(s.substring(j, k + 1));
     }
   }
 }
+
+/**
+ * performance: 2 ms < 100%, 40 MB < 100%
+ * time complexity:??
+ * space complexity: ??
+ */
