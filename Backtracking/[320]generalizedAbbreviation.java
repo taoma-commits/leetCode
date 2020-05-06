15561da967b2ea991624e75c19b29dc67655c9f4
taoma-commits <56742260+taoma-commits@users.noreply.github.com>
2020-05-06 10:12:24 -0400

    Update [320]generalizedAbbreviation.java

--- Backtracking/[320]generalizedAbbreviation.java
+++ Backtracking/[320]generalizedAbbreviation.java
@@ -31,4 +31,73 @@ class Solution {
  * for broader set of backtracking problems at the cost of extra memory
  * usage.
  */
+
+class Solution {
+  List<String> res = new ArrayList<>();
+  int n;
+  public List<String> generateAbbreviations(String word) {
+    n = word.length();
+    backtrack(word, 0, 0, new StringBuilder());
+    return res;
+  }
+
+  private void backtrack(String word, int i, int k, StringBuilder temp) {
+    int len = temp.length(); // keep track of length (could add multiple digits at once)  
+    if (i == n) {
+      if (k > 0) temp.append(k);
+      res.add(temp.toString());
+    } else {
+      backtrack(word, i + 1, k + 1, temp);
+      if (k > 0) {
+        temp.append(k);
+      }
+      temp.append(word.charAt(i));
+      backtrack(word, i + 1, 0, temp);
+    }
+    temp.setLength(len); // setLength() saves 1ms running time 
+  }
+}
+
+/**
+ * performance: 6 ms < 98%, 45 MB < 100%
+ * time complexity: same
+ * space complexity: same 
+ * notes: keep track of length of StringBuilder rather than delete the last added character.
+ * The latter one does not work because the numbers added could have multiple digits. 
+ */
+
+class Solution {
+  public List<String> generateAbbreviations(String word) {
+    List<String> res = new ArrayList<>();
+    int n = word.length();
+    for (int i = 0; i < (1 << n); i++) {
+      res.add(abbreviate(word, i));
+    }
+    return res;
+  }
+
+  private String abbreviate(String word, int i) {
+    int k = 0, n = word.length();
+    StringBuilder sb = new StringBuilder();
+    for (int j = 0; j < n + 1; j++, i >>= 1) {
+      if (j == n || (i & 1) == 0) {
+        if (k > 0) sb.append(k);
+        if (j < n) {
+          sb.append(word.charAt(j));
+          k = 0;
+        }
+      } else {
+        k++;
+      }
+    }
+    return sb.toString();
+  }
+}
+
+/**
+ * performance: 21 ms < 54%, 45 MB < 100%
+ * time complexity: same 
+ * space complexity: same
+ * notes: (Knuth) bit manipulation
+ */
  
