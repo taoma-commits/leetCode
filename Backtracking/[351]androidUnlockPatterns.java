9174b9ac17b31243aff5b331c1411327e3b41cf9
taoma-commits <56742260+taoma-commits@users.noreply.github.com>
2020-05-09 07:42:13 -0400

    Update [351]androidUnlockPatterns.java

--- Backtracking/[351]androidUnlockPatterns.java
+++ Backtracking/[351]androidUnlockPatterns.java
@@ -30,3 +30,71 @@ class Solution {
  * time complexity:
  * space complexity:
  */
+
+class Solution {
+  int count = 0;
+  int[][] grid = new int[10][10];
+  boolean[] visited = new boolean[10];
+
+  public int numberOfPatterns(int m, int n) {
+    grid[1][3] = 2;
+    grid[3][1] = 2;
+    grid[3][9] = 6;
+    grid[9][3] = 6;
+    grid[7][1] = 4;
+    grid[1][7] = 4;
+    grid[7][9] = 8;
+    grid[9][7] = 8;
+
+    for (int i = 1; i < 5; i++) {
+      grid[i][10 - i] = 5;
+      grid[10 - i][i] = 5;
+    }
+
+    int res = 0;
+
+    visited[1] = true;
+    backtrack(1, 1, m, n);
+    visited[1] = false;
+    res += count * 4;
+    count = 0;
+
+    visited[2] = true;
+    backtrack(2, 1, m, n);
+    visited[2] = false;
+    res += count * 4;
+    count = 0;
+    
+    visited[5] = true;
+    backtrack(5, 1, m, n);
+    visited[5] = false;
+    res += count;
+    return res;
+  }
+
+  private void backtrack(int i, int len, int m, int n) {
+    if (len == n) {
+      count++;
+      return;
+    }
+    if (len > m - 1) {
+      count++;
+    }
+    for (int j = 1; j < 10; j++) {
+      if (visited[j]) continue;
+      int g = grid[i][j];
+      if (g == 0 || visited[g]) {
+        visited[j] = true;
+        backtrack(j, len + 1, m, n);
+        visited[j] = false;
+      }
+    }
+  }
+}
+
+/**
+ * performance: 8 ms < 97%, 36.5 MB < 50%
+ * time complexity:
+ * space complexity: 
+ * notes: 
+ */
