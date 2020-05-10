2fc5f416528742d0e68135240cd354660d74b996
taoma-commits <56742260+taoma-commits@users.noreply.github.com>
2020-05-10 08:58:45 -0400

    Create [70]climbingStairs.java

---
+++ DP/[70]climbingStairs.java
@@ -0,0 +1,17 @@
+class Solution {
+  public int climbStairs(int n) {
+    int a = 1, b = 1, c = 1; 
+    for (int i = 2; i < n + 1; i++) {
+      c = a + b;
+      a = b;
+      b = c;
+    }
+    return c;
+  }
+}
+
+/**
+ * performance: 0 ms < 100%
+ * time complexity: O(n)
+ * space complexity: O(1)
+ */
