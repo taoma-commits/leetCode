d1b63d2c292a959b9ccb5662dd676ab661a9267a
Tao Ma <56742260+taoma-commits@users.noreply.github.com>
2020-05-11 16:50:53 -0400

    Update [140]wordBreakII.java

--- Backtracking/[140]wordBreakII.java
+++ Backtracking/[140]wordBreakII.java
@@ -1,6 +1,7 @@
 class Solution {
   String S;
   int n;
+  int len = 0;
   Set<String> set;
   Map<Integer, List<String>> map = new HashMap<>();
 
@@ -8,6 +9,9 @@ class Solution {
     S = s;
     n = s.length();
     set = new HashSet<String>(wordDict);
+    for (String word : wordDict) {
+      len = Math.max(len, word.length());
+    }
     return backtrack(0);
   }
 
@@ -20,7 +24,7 @@ class Solution {
     if (map.containsKey(i)) {
       return map.get(i);
     }
-    for (int j = i + 1; j < n + 1; j++) {
+    for (int j = i + 1; j < n + 1 && j < i + len + 1; j++) {
       if (set.contains(S.substring(i, j))) {
         for (String str : backtrack(j)) {
           if (str.length() > 0) {
@@ -36,7 +40,8 @@ class Solution {
 }
 
 /**
- * performance: 11 ms < 76%, 40 MB < 7%
+ * performance: 4 ms < 97%
  * time complexity: O(n^2) ??
  * space complexity: O(n^2) ??
+ * notes: key optimization is the purning done by adding condition j < i + len + 1.
  */
