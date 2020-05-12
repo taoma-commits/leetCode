c5b0a2380181b41de290b436dd6feb3bf1b1935b
Tao Ma <56742260+taoma-commits@users.noreply.github.com>
2020-05-11 22:45:30 -0400

    Update [140]wordBreakII.java

--- Backtracking/[140]wordBreakII.java
+++ Backtracking/[140]wordBreakII.java
@@ -41,7 +41,42 @@ class Solution {
 
 /**
  * performance: 4 ms < 97%
- * time complexity: O(n^2) ??
- * space complexity: O(n^2) ??
+ * time complexity: O(n^3), 1^2 + 2^2 + ... + n^2 ~ n^3.
+ * space complexity: O(n^3) same as time complexity. 
  * notes: key optimization is the purning done by adding condition j < i + len + 1.
+ */
+
+class Solution {
+  public List<String> wordBreak(String s, List<String> wordDict) {
+    int n = s.length();
+    int len = 0;
+    for (String word : wordDict) {
+      len = Math.max(len, word.length());
+    }
+    ArrayList<String>[] dp = new ArrayList[n + 1];
+    dp[0] = new ArrayList<String>();
+    dp[0].add("");
+    for (int i = 1; i < n + 1; i++) {
+      ArrayList<String> list = new ArrayList<>();
+      for (int j = 1; j < len + 1 && j < i + 1; j++) {
+        if (dp[i - j].size() > 0 && wordDict.contains(s.substring(i - j, i))) {
+          for (String str : dp[i - j]) {
+            if (str.length() > 0) {
+              str = str + " ";
+            }
+            list.add(str + s.substring(i - j, i));
+          }
+        }
+      }
+      dp[i] = list;
+    }
+    return dp[n];
+  }
+}
+
+/**
+ * performance: TLE
+ * time complexity: O(n^3)
+ * space complexity: O(n^3)
+ * notes: why DP doesn't work? 
  */
