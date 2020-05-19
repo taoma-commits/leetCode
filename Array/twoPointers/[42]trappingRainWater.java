587bd05b94593d607ebe01673d91be0cd99c4678
Tao Ma <56742260+taoma-commits@users.noreply.github.com>
2020-05-19 17:04:54 -0400

    Update [42]trappingRainWater.java

--- Array/twoPointers/[42]trappingRainWater.java
+++ Array/twoPointers/[42]trappingRainWater.java
@@ -31,35 +31,93 @@ class Solution {
 
 /*
 * performance: 1 ms < 100%, 43.6 MB < 5%.
-* tag: two pointers
-* notes: 1. left (right) pointer must stop when current height is equal to lh (rh)!
-         2. could not figure this out in hours: assign extra lh and rh is NATRUAL!
-         3. time complexity = O(n); space complexity = O(1).
-* companies: Amazon(53), Goldman-Sachs, Microsoft, Facebook.
+* time complexity: O(n)
+* space complexity: O(1)
+* notes: pay attention to < and <= to avoid out of bound error.
 */
 
 class Solution {
-    public int trap(int[] height) {
-        int water = 0;
-        Stack<Integer> stack = new Stack<>();
-        for (int i = 0; i < height.length; i++) {
-            while (!stack.empty() && height[i] > height[stack.peek()]) {
-                int b = stack.pop();
-                if (stack.empty()) {
-                    break;
-                }
-                int l = stack.peek();
-                water += (Math.min(height[l], height[i]) - height[b]) * (i - l - 1);
-            }
-            stack.push(i);
+  public int trap(int[] height) {
+    int n = height.length;
+    int lh = 0;
+    int rh = 0;
+    int volumn = 0;
+    int l = 0;
+    int r = n - 1;
+    while (l <= r) {
+      if (lh < rh) {
+        while (height[l] <= lh) {
+          volumn += lh - height[l++];
         }
-        return water;
+        lh = height[l++];
+      } else {
+        while (height[r] < rh) {
+          volumn += rh - height[r--];
+        }
+        rh = height[r--];
+      }
     }
+    return volumn;
+  }
 }
 
 /**
-* performance: 4 ms < 34%, 46.3 < 5%.
-* notes:
-        1. the idea is to compute each horizontal water strip not vertical ones.
-        2. implement by stack
+ * performance: 1 ms < 100%, 39 MB < 13%
+ * time complexity: same
+ * space complexity: same
+ */
+
+class Solution {
+  public int trap(int[] height) {
+    int water = 0;
+    Deque<Integer> stack = new ArrayDeque<>();
+    for (int i = 0; i < height.length; i++) {
+      while (!stack.isEmpty() && height[i] > height[stack.peekLast()]) {
+        int j = stack.pollLast();
+        if (stack.isEmpty()) {
+          break;
+        }
+        int k = stack.peekLast();
+        water += (Math.min(height[k], height[i]) - height[j]) * (i - k - 1);
+      }
+      stack.addLast(i);
+    }
+    return water;
+  }
+}
+
+/**
+* performance: 2 ms < 46%
+* time complexity: O(n), each bar is pushed and poped once.
+* space complexity: O(n)
+* notes: 1. compute each horizontal water strip instead of vertical ones. 
+* 2. monotonic stack
 */
+
+class Solution {
+  public int trap(int[] height) {
+    int n = height.length;
+    int[] left = new int[n];
+    int[] right = new int[n];
+    int max_left = 0;
+    int max_right = 0; 
+    for (int i = 0; i < n; i++) {
+      max_left = Math.max(max_left, height[i]);
+      max_right = Math.max(max_right, height[n - i - 1]);
+      left[i] = max_left;
+      right[n - i - 1] = max_right;
+    }
+    int volumn = 0;
+    for (int i = 0; i < n; i++) {
+      volumn += Math.min(left[i], right[i]) - height[i];
+    }
+    return volumn;
+  }
+}
+
+/**
+ * performance: 1 ms < 100%
+ * time complexity: O(n)
+ * space complexity: O(n)
+ * notes: left and right DP. 
+ */
