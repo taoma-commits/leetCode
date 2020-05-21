8d4e4f24252969129cab08edf77910f4830ca137
Tao Ma <56742260+taoma-commits@users.noreply.github.com>
2020-05-21 15:37:51 -0400

    Update [198]houseRobber.java

--- [198]houseRobber.java
+++ [198]houseRobber.java
@@ -1,22 +1,18 @@
-/*
-Runtime: 0 ms, faster than 100.00% of Java online submissions for House Robber.
-Memory Usage: 34 MB, less than 100.00% of Java online submissions for House Robber.
-@dynamicProgramming
-*/
-
 class Solution {
-    public int rob(int[] nums) {
-        int len = nums.length;
-        if (len == 0) return 0;
-        if (len == 1) return nums[0];
-        int sum, former;
-        former = sum = 0;
-        int latter = nums[0];
-        for (int i = 1; i < len; i++) {
-            sum = former + nums[i];
-            former = latter;
-            latter = Math.max(sum, latter);
-        }
-        return latter;
+  public int rob(int[] nums) {
+    int prev = 0; 
+    int current = 0;
+    for (int i = 0; i < nums.length; i++) {
+      int temp = current;
+      current = Math.max(current, prev + nums[i]);
+      prev = temp;
     }
+    return current;
+  }
 }
+
+/**
+ * performance: 0 ms < 100%
+ * time complexity: O(n)
+ * space complexity: O(1)
+ */
