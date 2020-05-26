b7bc5d8398501818c8eff5260fda7e95b8a10897
Tao Ma <56742260+taoma-commits@users.noreply.github.com>
2020-05-25 22:27:56 -0400

    Update [287]findDuplicateNumber.java

--- Array/binarySearch/[287]findDuplicateNumber.java
+++ Array/binarySearch/[287]findDuplicateNumber.java
@@ -1,30 +1,55 @@
 class Solution {
-    public int findDuplicate(int[] nums) {
-        int lo = 1;
-        int hi = nums.length - 1;
-
-        while (lo < hi) {
-            int mid = (lo + hi) / 2;
-            int count = 0;
-            for (int num : nums) {
-                if (num <= mid) {
-                    count++;
-                }
-            }
-            if (count > mid) {
-                hi = mid;
-            } else {
-                lo = mid + 1;
-            }
+  public int findDuplicate(int[] nums) {
+    int n = nums.length;
+    int l = 1;
+    int r = n;
+    while (l < r) {
+      int mid = (l + r) >> 1; 
+      int count = 0;
+      for (int i = 0; i < n; i++) {
+        if (nums[i] <= mid) {
+          count++;
         }
-        return lo;
+      }
+      if (count <= mid) {
+        l = mid + 1;
+      } else {
+        r = mid;
+      }
     }
+    return r;
+  }
 }
 
 /**
-* performance: 5 ms < 33%, 48 MB < 5%.
-* notes: time complexity = O(nlogn), space complexity = O(1)
-* companies: Amazon, Microsoft, Google.
-*/
+ * performance: 3 ms < 60%
+ * time complexity = O(n * log(n))
+ * space complexity = O(1)
+ * notes: binary search in numbers 1 to n
+ */
 
-// best solution: find the cycle, then find cycle entrance. 
+class Solution {
+  public int findDuplicate(int[] nums) {
+    int fast = 0;
+    int slow = 0;
+    while (true) {
+      fast = nums[nums[fast]];
+      slow = nums[slow];
+      if (fast == slow) break; 
+    }
+    slow = 0;
+    while (fast != slow) {
+      fast = nums[fast];
+      slow = nums[slow];
+    }
+    return slow;
+  }
+}
+
+/**
+ * performance: 0 ms < 100%
+ * time complexity: O(n) because the 1st meet of two pointers will happen before slow pointer have visited all numbers in the cycle. 
+ * space complexity: O(1)
+ * notes: Floyd's slow and fast pointers; 
+ */
+
