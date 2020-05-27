a13ee88d4435f8f8a58a3cd966526a022e6f3160
Tao Ma <56742260+taoma-commits@users.noreply.github.com>
2020-05-27 15:33:04 -0400

    Update [243]shortestWordDistance.java

--- Array/twoPointers/[243]shortestWordDistance.java
+++ Array/twoPointers/[243]shortestWordDistance.java
@@ -17,13 +17,8 @@ class Solution {
 
 /*
 * performance: 2ms < 98.12%; 36.1 MB < 91.70%.
-* notes:
-      1. compare strings: string1.equals(string2)
-      2. two pointers
-      3. idx1 and idx2 start at -1 instead of 0
-      4. use hash table would be slower and use extra memory.
-        Not worth it if only compute distance for one pair.
-* companies: LinkedIn 8, Oracle 2
+* time complexity: O(n)
+* space complexity: O(1)
 */
 
 class Solution {
@@ -40,8 +35,5 @@ class Solution {
 }
 
 /*
-* A solution with less code
-* notes:
-      1. one has to carefully set the initial values of idx1 and idx2
-      2. it does not do more compares than the first solution: Math.min replaces comparing idx1 and idx2
+* same 
 */
