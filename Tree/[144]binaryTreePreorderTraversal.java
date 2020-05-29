f616125cad93673593cf1a0e43b2e4a6c7a42895
Tao Ma <56742260+taoma-commits@users.noreply.github.com>
2020-05-29 16:01:24 -0400

    Update [144]binaryTreePreorderTraversal.java

--- Tree/[144]binaryTreePreorderTraversal.java
+++ Tree/[144]binaryTreePreorderTraversal.java
@@ -16,3 +16,40 @@ class Solution {
  * space complexity: O(depth) 
  * notes: recursion. 
  */
+
+
+
+class Solution {
+  public List<Integer> preorderTraversal(TreeNode root) {
+    List<Integer> res = new ArrayList<>();
+    TreeNode node = root;
+    TreeNode pre;
+    while (node != null) {
+      if (node.left == null) {
+        res.add(node.val);
+        node = node.right;
+      } else {
+        pre = node.left;
+        while (pre.right != null && pre.right != node) {
+          pre = pre.right;
+        }
+        if (pre.right == null) {
+          res.add(node.val);
+          pre.right = node;
+          node = node.left;
+        } else {
+          node = node.right;
+          pre.right = null;
+        }
+      }
+    }
+    return res;
+  }
+}
+
+/**
+ * performance: 0 ms 
+ * time complexity: O(n)
+ * space complexity: O(1)
+ * notes: Morris traversal 
+ */
