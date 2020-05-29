f5519f197f3c4c54ae6a422cd264eaef85f347ba
Tao Ma <56742260+taoma-commits@users.noreply.github.com>
2020-05-29 11:24:46 -0400

    Update [45]jumpGameII.java

--- Array/greedy/[45]jumpGameII.java
+++ Array/greedy/[45]jumpGameII.java
@@ -6,29 +6,30 @@ neighbors of previous nodes are visited.
 */
 
 class Solution {
-    public int jump(int[] nums) {
-        int end = 0, reach = 0, steps = 0;
-        for (int i = 0; i < nums.length - 1; i++) { // note 2
-            reach = Math.max(nums[i] + i, reach);
-            if (i == end) {
-                end = reach;
-                reach = 0;
-                steps++;
-            }
-        }
-        return steps;
+  public int jump(int[] nums) {
+    int steps = 0;
+    int max = 0;
+    int maxNext = 0;
+    for (int i = 0; i < nums.length - 1; i++) {
+      maxNext = Math.max(nums[i] + i, maxNext);
+      if (i == max) {
+        steps++;
+        max = maxNext;
+      }
     }
+    return steps;
+  }
 }
 
 /*
-* performance: 2 ms < 95%, 37.7 MB < 77%.
+* performance: 2 ms < 95%
+* time complexity: O(n)
+* space complexity: O(1)
 * notes:
       1. greedy algorithm finds the global optimum: from one widest step to the next one includes
         all other possibilities.
       2. i < nums.length - 1 avoids comparison end = Math.max(reach, nums.length - 1) and the algorithm
         returns steps not steps - 1. As a consequence, the algorithm is 1 ms faster.
-      3. time complexity = O(n), space complexity = O(1).
-* companies: Amazon, Facebook, Google.
 */
 
 class Solution {
@@ -56,23 +57,20 @@ class Solution {
 */
 
 class Solution {
-    public int jump(int[] nums) {
-        int len = nums.length;
-        int[] memo = new int[len];
-        memo[0] = 0;
-        for (int i = 1; i < len; i++) {
-            memo[i] = Integer.MAX_VALUE;
-        }
-        for (int i = 1; i < len; i++) {
-            for (int j = 0; j < i; j++) {
-                if (memo[j] != Integer.MAX_VALUE && nums[j] + j >= i) {
-                    memo[i] = Math.min(memo[i], memo[j] + 1);
-                }
-            }
-        }
-        return memo[len - 1];
+  public int jump(int[] nums) {
+    int n = nums.length;
+    int[] distTo = new int[n];
+    Arrays.fill(distTo, n);
+    distTo[0] = 0;
+    for (int i = 0; i < n - 1; i++) {
+      for (int j = i + 1; j <= i + nums[i] && j < n; j++) {
+        distTo[j] = Math.min(distTo[i] + 1, distTo[j]);
+      }
     }
+    return distTo[n - 1];
+  }
 }
-/* performance: exceeds time limit
-* notes: DP solution
+
+/* performance: 530 ms 
+* notes: topological sort algorithm for shortest paths on DAGs. 
 */
