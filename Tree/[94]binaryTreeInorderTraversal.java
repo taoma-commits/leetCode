e90fa9f2572d14f2f04b975a626adb77bf12e56d
Tao Ma <56742260+taoma-commits@users.noreply.github.com>
2020-05-29 18:44:46 -0400

    Update [94]binaryTreeInorderTraversal.java

--- Tree/[94]binaryTreeInorderTraversal.java
+++ Tree/[94]binaryTreeInorderTraversal.java
@@ -41,5 +41,38 @@ class Solution {
 /**
  * same
  * notes: replacing recursion with a stack
- */ 
+ */
+
+class Solution {
+  public List<Integer> inorderTraversal(TreeNode root) {
+    List<Integer> res = new ArrayList<>();
+    TreeNode node = root;
+    while (node != null) {
+      if (node.left == null) {
+        res.add(node.val);
+        node = node.right;
+      } else {
+        TreeNode pre = node.left;
+        while (pre.right != null && pre.right != node) {
+          pre = pre.right;
+        }
+        if (pre.right == null) {
+          pre.right = node;
+          node = node.left;
+        } else {
+          res.add(node.val);
+          pre.right = null;
+          node = node.right;
+        }
+      }
+    }
+    return res;
+  }
+}
+
+/**
+ * same 
+ * notes: Morris traversal 
+ */
+ 
  
