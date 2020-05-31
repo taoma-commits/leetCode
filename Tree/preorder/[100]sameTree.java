3ad76060f801c46e6ef663d937cbdba9ad20b067
Tao Ma <56742260+taoma-commits@users.noreply.github.com>
2020-05-31 17:19:58 -0400

    Update [100]sameTree.java

--- Tree/preorder/[100]sameTree.java
+++ Tree/preorder/[100]sameTree.java
@@ -17,3 +17,39 @@ class Solution {
  * notes: recursion.
  */
  
+class Solution {
+  public boolean isSameTree(TreeNode pNode, TreeNode qNode) {
+    if (pNode == null && qNode == null) return true;
+    if (!checkNodes(pNode, qNode)) return false;
+    Deque<TreeNode> P = new ArrayDeque<>();
+    Deque<TreeNode> Q = new ArrayDeque<>();
+    P.addLast(pNode);
+    Q.addLast(qNode);
+    while (!P.isEmpty() && !Q.isEmpty()) {
+      TreeNode p = P.removeLast();
+      TreeNode q = Q.removeLast();
+      if (!checkNodes(p.left, q.left)) return false;
+      if (!checkNodes(p.right, q.right)) return false;
+      if (p.left != null && q.left != null) {
+        P.addLast(p.left);
+        Q.addLast(q.left);
+      }
+      if (p.right != null && q.right != null) {
+        P.addLast(p.right);
+        Q.addLast(q.right);
+      }
+    }
+    return P.isEmpty() && Q.isEmpty();
+  }
+
+  private boolean checkNodes(TreeNode p, TreeNode q) {
+    if (p == null && q == null) return true;
+    if (p == null || q == null) return false; 
+    return p.val == q.val;
+  }
+}
+
+/**
+ * same
+ * notes: double stacks replacing recursion. 
+ */
